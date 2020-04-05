c7a3004be29020a266d4460a13a03a8ca0561e60
khanzzirfan <khanzz_irfan@hotmail.com>
2020-04-05 13:23:17 +1000

    chore

--- src/containers/AddTodo.jsx
+++ src/containers/AddTodo.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addTodo } from '../actions';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
