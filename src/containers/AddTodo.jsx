67f2e6a572754b6a65ca5088c03e8ad32024e97d
khanzzirfan <khanzz_irfan@hotmail.com>
2020-04-28 05:17:40 +1000

    chore: notes

--- src/containers/AddTodo.jsx
+++ src/containers/AddTodo.jsx
@@ -1,56 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { addTodo } from '../actions';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
-class AddTodo extends Component {
-  state = {
-    value: null,
+const AddTodo = ({ dispatch }) => {
+  const [value, setValue] = useState(null);
+
+  const handleOnChange = (event) => {
+    setValue(event.target.value);
   };
 
-  handleOnChange = (event) => {
-    this.setState({ value: event.target.value });
-  };
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { value } = this.state;
-    const { dispatch } = this.props;
     dispatch(addTodo(value));
-    this.setState({ value: null });
+    setValue(null);
   };
 
-  render() {
-    const { value } = this.state;
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <div className="d-flex flex-row">
-            <Typography variant="h1" component="h2" gutterBottom color="primary">
-              todos
-            </Typography>
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="d-flex flex-row">
+          <Typography variant="h1" component="h2" gutterBottom color="primary">
+            todos
+          </Typography>
+        </div>
+        <div className="d-flex flex-column mx-2">
+          <TextField
+            required
+            id="todo"
+            label="What needs to be done?"
+            value={value || ''}
+            onChange={handleOnChange}
+            className="my-2"
+          />
+          <div className="my-2">
+            <Button variant="contained" color="primary" type="submit">
+              Add
+            </Button>
           </div>
-          <div className="d-flex flex-column mx-2">
-            <TextField
-              required
-              id="todo"
-              label="What needs to be done?"
-              value={value || ''}
-              onChange={this.handleOnChange}
-              className="my-2"
-            />
-            <div className="my-2">
-              <Button variant="contained" color="primary" type="submit">
-                Add
-              </Button>
-            </div>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default connect()(AddTodo);
