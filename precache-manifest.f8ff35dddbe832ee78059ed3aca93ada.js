41cd64a695db7991b840040a19f9109750ce0367
khanzzirfan <khanzz_irfan@hotmail.com>
2020-08-28 14:33:09 +1000

    Updates

---
+++ precache-manifest.f8ff35dddbe832ee78059ed3aca93ada.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d981567253da3028296965c284704698",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb893e92e8cbcd172c9c",
+    "url": "/static/css/2.e89e737b.chunk.css"
+  },
+  {
+    "revision": "b41ee22b053c51966709",
+    "url": "/static/css/main.405a14fb.chunk.css"
+  },
+  {
+    "revision": "eb893e92e8cbcd172c9c",
+    "url": "/static/js/2.fb913056.chunk.js"
+  },
+  {
+    "revision": "b948d2a7727b83687a00ec53ef5267c0",
+    "url": "/static/js/2.fb913056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b41ee22b053c51966709",
+    "url": "/static/js/main.63d46e70.chunk.js"
+  },
+  {
+    "revision": "0879bed73fba7b62986a",
+    "url": "/static/js/runtime-main.416eea82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
